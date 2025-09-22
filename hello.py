93e4733af505ab3563dcc7dfb04dd2fc159df631
Niraaj Rajalingam <niraaj@Niraajs-MacBook.local>
2025-09-22 01:35:27 -0400

    adding email

--- hello.py
+++ hello.py
@@ -4,28 +4,39 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'qwertyuiopasdfghjklzxcvbnm'
 
-class NameForm(FlaskForm):
+def validate_email(form, field):
+        if "@" not in field.data:
+            raise ValidationError("Please include an '@' in the email address. '{field.data}' is missing an '@'.")
+
+class Form(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your email?', validators=[DataRequired(), validate_email])
     submit = SubmitField('Submit')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = Form()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html',
-        form = form, name = session.get('name'))
+        form=form, name=session.get('name'),
+        email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
